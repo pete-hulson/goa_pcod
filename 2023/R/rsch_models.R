e1b4d760a9c672850c29b1557ac89de0ac37a616
pete-hulson <pete.hulson@noaa.gov>
2023-08-25 10:32:01 -0800

    start on env link growth model

--- 2023/R/rsch_models.R
+++ 2023/R/rsch_models.R
@@ -16,6 +16,7 @@ lapply(libs, library, character.only = TRUE)
 # model names
 base_mdl <- "2019.1a-2022" # 2022 accepted model
 new_mdl1 <- "2019.1b-2022" # 2022 model with minsamplesize correction
+new_mdl2 <- "2023.1-2022" # 2022 model with env growth link
 
 # assessment year
 ass_yr <- as.numeric(format(Sys.Date(), format = "%Y"))
@@ -61,8 +62,52 @@ mdl1_res <- r4ss::SS_output(dir = here::here(ass_yr, 'rsch', new_mdl1),
                             printstats = TRUE)
 
 
+# model 2023.1 analysis (env link for growth)
 
-all_summ <- r4ss::SSsummarize(list(base_res, mdl1_res))
+# copy over the stock synthesis model files to the new directory for model 2019.1b
+r4ss::copy_SS_inputs(dir.old = here::here(ass_yr, 'rsch', base_mdl), 
+                     dir.new = here::here(ass_yr, 'rsch', new_mdl2),
+                     overwrite = TRUE)
+base::file.copy(from = here::here(ass_yr, 'rsch', base_mdl, "ss.exe"),
+                to = here::here(ass_yr, 'rsch', new_mdl2, "ss.exe"),
+                overwrite = TRUE)
+base::file.copy(from = here::here(ass_yr, 'rsch', base_mdl, "ss.par"),
+                to = here::here(ass_yr, 'rsch', new_mdl2, "ss.par"),
+                overwrite = TRUE)
+
+
+
+# add env link to ctl file - something doesn't work here, changing it manually
+
+# ctl <- r4ss::SS_readctl(here::here(ass_yr, 'rsch', base_mdl, list.files(here::here(ass_yr, 'rsch', base_mdl), pattern = 'ctl')),
+#                         datlist = here::here(ass_yr, 'rsch', base_mdl, list.files(here::here(ass_yr, 'rsch', base_mdl), pattern = 'dat')[2]))
+# 
+# ctl$MG_parms$`env_var&link`[2:3] = 101
+# 
+# r4ss::SS_writectl(ctllist = ctl,
+#                   outfile = here::here(ass_yr, 'rsch', new_mdl1, list.files(here::here(ass_yr, 'rsch', new_mdl2), pattern = 'ctl')),
+#                   overwrite = TRUE)
+
+
+# run model
+r4ss::run_SS_models(dirvec = here::here(ass_yr, 'rsch', new_mdl2),
+                    skipfinished = FALSE,
+                    intern = TRUE)
+
+# read the model output and print diagnostic messages
+mdl2_res <- r4ss::SS_output(dir = here::here(ass_yr, 'rsch', new_mdl2),
+                            verbose = TRUE,
+                            printstats = TRUE)
+
+
+
+
+
+
+# plots results and stuff ----
+
+
+all_summ <- r4ss::SSsummarize(list(base_res, mdl1_res, mdl2_res))
 
 names(all_summ)
 
