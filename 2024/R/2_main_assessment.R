187dac3c5846693ec1bd7684e36e48d40277c6f6
pete-hulson <pete.hulson@noaa.gov>
2024-10-11 11:14:50 -0800

    set up jitter anyls

--- 2024/R/2_main_assessment.R
+++ 2024/R/2_main_assessment.R
@@ -1,6 +1,24 @@
 ## Script to run 2024 GOA Pacific Cod Assessment (P. Hulson)
 
 
+# user-defined function arguments ----
+
+## ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))<
+## ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))<
+
+# day data pulled
+dat_day <- "Oct10"
+
+# 2023 accepted model
+base_mdl <- "2019.1b-2023"
+
+# full run for retro/jitter/mcmc/etc
+full_run = FALSE
+
+## ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))<
+## ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))< ~~~~ <*)))<
+
+
 # load necessary packages ----
 ## cran packages ----
 pkg_cran <- c("data.table",
@@ -37,12 +55,6 @@ lapply(pkg_git, library, character.only = TRUE)
 
 # define run parameters/load fcns ----
 
-# day data pulled
-dat_day <- "Oct10"
-
-# 2023 accepted model
-base_mdl <- "2019.1b-2023"
-
 # current assessment year
 new_year <- as.numeric(format(Sys.Date(), format = "%Y"))
 
@@ -62,7 +74,6 @@ if(!file.exists(here::here(new_year, 'mgmt', base_mdl_update, 'ss3.exe'))){
   start_ss_fldr(from = here::here(new_year - 1, 'mgmt', base_mdl),
                 to = here::here(new_year, 'mgmt', base_mdl_update))
 }
-
 
 # update files
 update_ss3_files(new_year, 
@@ -87,7 +98,7 @@ run_ss3_model(new_year,
 # - plus length bin set at 104 cm
 # - correct season for twl survey caal
 # - turn off recr forecase phase
-# - fic rec forecast option
+# - fix rec forecast option
 # - update l-w
 
 new_base <- "2019.1c-2024"
@@ -209,9 +220,9 @@ write.csv(base_mdl_update_mscen$Two_year, here::here(new_year, "output", "mscen"
 # run retrospective analysis ----
 
 # define how many retro years you want to go back
-ret_yr <- 1 # For testing
-# ret_yr <- 10 # For full
-
+if(isTRUE(full_run)){
+  ret_yr <- 10
+} else{ret_yr <- 1}
 
 ## 2019.1b ----
 r4ss::retro(dir = here::here(new_year, "mgmt", base_mdl_update),
@@ -241,9 +252,12 @@ save(retrosumm_1e5cm, file = here::here(new_year, "output", "retro", "retrosumm_
 # run leave-one-out analysis ----
 
 ## across time ----
-# Define how many LOO years you want to go back
-loo_yr <- 1 # For testing
-# loo_yr <- 10 # For full
+
+# define how many loo years you want to go back
+if(isTRUE(full_run)){
+  loo_yr <- 10
+} else{loo_yr <- 1}
+
 loo_year <- year_loo(dir = here::here(new_year, "mgmt", new_base_lcomp_bin5),
                      years = 0:-loo_yr,
                      cyr = new_year)
@@ -261,39 +275,34 @@ save(loo_data, file = here::here(new_year, "output", "loo", "loo_data.RData"))
 write.csv(loo_year[[1]], here::here(new_year, "output", "loo", "loo_year_table.csv"))
 
 
-# Run Jitter ----
-# note, in 2023 assessment couldn't get r4ss::jitter to run, but, could get the functions to run
-# so just sourcing functions and will come back to this
+# run jitter ----
 
-source(here::here(new_year, "R", "assessment", "jitter.r"))
+# define how many jitters you want to go back
+if(isTRUE(full_run)){
+  Njitter <- 50
+} else{Njitter <- 5}
 
-# Define how many jitters you want to do
-Njitter <- 5 # For testing
-# Njitter <- 50 # For full
-
+## set up jitter model ----
 # define a new directory
-if (!file.exists(here::here(new_year, "mgmt", Model_name_new, "jitter"))) 
-  dir.create(here::here(new_year, "mgmt", Model_name_new, "jitter"))
-
-jitter_dir <- here::here(new_year, "mgmt", Model_name_new, "jitter")
-
-# copy over the stock synthesis model files to the new directory
-r4ss::copy_SS_inputs(dir.old = here::here(new_year, "mgmt", Model_name_new), 
-                     dir.new = here::here(new_year, "mgmt", Model_name_new, "jitter"),
+if (!file.exists(here::here(new_year, "mgmt", new_base_lcomp_bin5, "jitter"))) 
+  dir.create(here::here(new_year, "mgmt", new_base_lcomp_bin5, "jitter"))
+# copy ss3 files
+r4ss::copy_SS_inputs(dir.old = here::here(new_year, "mgmt", new_base_lcomp_bin5), 
+                     dir.new = here::here(new_year, "mgmt", new_base_lcomp_bin5, "jitter"),
                      copy_par = TRUE,
                      copy_exe = TRUE,
                      overwrite = TRUE)
 
-# run the jitters
-jitter_loglike <- jitter(dir = jitter_dir,
-                         Njitter = Njitter,
-                         jitter_fraction = 0.05,
-                         init_values_src = 1,
-                         exe = "ss3",
-                         printlikes = TRUE,
-                         verbose = FALSE)
+## run the jitters ----
+jitter_loglike <- r4ss::jitter(dir = here::here(new_year, "mgmt", new_base_lcomp_bin5, "jitter"),
+                               Njitter = Njitter,
+                               jitter_fraction = 0.05,
+                               init_values_src = 1,
+                               exe = "ss3",
+                               printlikes = TRUE,
+                               verbose = FALSE)
 
-# Save output
+## save results ----
 write.csv(jitter_loglike, here::here(new_year, "output", "jitter_table.csv"))
 
 
@@ -305,244 +314,3 @@ write.csv(jitter_loglike, here::here(new_year, "output", "jitter_table.csv"))
 
 
 
-
-
-
-
-
-
-
-# Current model name
-Model_name_old <- "2019.1a-2023"
-Model_name_new <- "2019.1b-2023"
-
-# Current assessment year
-new_year <- as.numeric(format(Sys.Date(), format = "%Y"))
-
-
-# Run models and management scenarios ----
-
-# set up forecast file
-forecast <- r4ss::SS_readforecast(file = here::here(new_year, "mgmt", Model_name_new, 'forecast.ss'))
-
-forecast$Bmark_years <- c(1977, new_year - 2, 1977, new_year - 2, 1977, new_year - 1, 1977, new_year - 2, 1977, new_year - 2)
-
-forecast$Fcast_years <- c(2000, new_year - 2, new_year - 5, new_year - 1, 1977, new_year - 2)
-
-r4ss::SS_writeforecast(mylist = forecast,
-                      dir = here::here(new_year, "mgmt", Model_name_old),
-                      overwrite = TRUE)
-
-r4ss::SS_writeforecast(mylist = forecast,
-                       dir = here::here(new_year, "mgmt", Model_name_new),
-                       overwrite = TRUE)
-
-
-# Run previous accepted model (first run with init vals, then set to par for subsequent runs)
-
-# set up starter file
-old_starter <- r4ss::SS_readstarter(file = here::here(new_year, "mgmt", Model_name_old, 'starter.ss'))
-
-old_starter$init_values_src = 0
-
-r4ss::SS_writestarter(mylist = old_starter,
-                      dir = here::here(new_year, "mgmt", Model_name_old),
-                      overwrite = TRUE)
-
-r4ss::run(dir = here::here(new_year, "mgmt", Model_name_old),
-          skipfinished = FALSE,
-          show_in_console = TRUE)
-
-old_starter$init_values_src = 1
-  
-r4ss::SS_writestarter(mylist = old_starter,
-                      dir = here::here(new_year, "mgmt", Model_name_old),
-                      overwrite = TRUE)
-
-model_run_old <- r4ss::SS_output(dir = here::here(new_year, "mgmt", Model_name_old),
-                                 verbose = TRUE,
-                                 printstats = FALSE)
-
-# Run recommended model (first run with init vals, then set to par for subsequent runs)
-new_starter <- r4ss::SS_readstarter(file = here::here(new_year, "mgmt", Model_name_new, 'starter.ss'))
-
-new_starter$init_values_src = 0
-
-r4ss::SS_writestarter(mylist = new_starter,
-                      dir = here::here(new_year, "mgmt", Model_name_new),
-                      overwrite = TRUE)
-
-r4ss::run(dir = here::here(new_year, "mgmt", Model_name_new),
-          skipfinished = FALSE,
-          show_in_console = TRUE)
-
-new_starter$init_values_src = 1
-
-r4ss::SS_writestarter(mylist = new_starter,
-                      dir = here::here(new_year, "mgmt", Model_name_new),
-                      overwrite = TRUE)
-
-model_run_new <- r4ss::SS_output(dir = here::here(new_year, "mgmt", Model_name_new),
-                                 verbose = TRUE,
-                                 printstats = FALSE)
-
-# Run management scenarios
-
-source(here::here(new_year, "R", "assessment", "run_mngmnt_scenarios.r"))
-
-mscen_old <- Do_AK_Scenarios(Model_name = Model_name_old,
-                             Model_dir = here::here(new_year, "mgmt", Model_name_old),
-                             CYR = new_year,
-                             SYR = 1977,
-                             FCASTY = 15,
-                             FLEETS = c(1:3),
-                             do_fig = FALSE,
-                             SEXES = 1)
-
-mscen <- Do_AK_Scenarios(Model_name = Model_name_new,
-                         Model_dir = here::here(new_year, "mgmt", Model_name_new),
-                         CYR = new_year,
-                         SYR = 1977,
-                         FCASTY = 15,
-                         FLEETS = c(1:3),
-                         do_fig = TRUE,
-                         SEXES = 1)
-
-# Get ssb and index fit for spreadsheets with figures
-model_run_new$timeseries$Yr %>% 
-  bind_cols(model_run_new$timeseries$SpawnBio / 2) %>% 
-  rename(year = ...1, ssb = ...2) %>% 
-  filter(year >= 1977,
-         year <= new_year + 1) -> ssb_pred
-
-model_run_new$timeseries$Yr %>% 
-  bind_cols(model_run_new$timeseries$Bio_all) %>% 
-  rename(year = ...1, biom = ...2) %>% 
-  filter(year >= 1984,
-         year <= new_year + 1) -> tot_trwl_pred
-
-# Save output
-save(model_run_new, file = here::here(new_year, "output", "model_run.RData"))
-write.csv(ssb_pred, here::here(new_year, "output", "ssb_pred.csv"))
-write.csv(tot_trwl_pred, here::here(new_year, "output", "tot_trwl_pred.csv"))
-
-save(mscen, file = here::here(new_year, "output", "mgmnt_scen.RData"))
-write.csv(mscen$Tables, here::here(new_year, "output", "mgmnt_scen_table.csv"))
-write.csv(mscen$Two_year, here::here(new_year, "output", "mgmnt_exec_summ.csv"))
-write.csv(mscen_old$Two_year, here::here(new_year, "output", "mgmnt_exec_summ_old.csv"))
-
-
-# Run retrospective analysis ----
-
-# Define how many retro years you want to go back
-# ret_yr <- 1 # For testing
-ret_yr <- 10 # For full
-
-# Run retrospective
-r4ss::retro(dir = here::here(new_year, "mgmt", Model_name_new),
-            years = 0:-ret_yr)
-
-# load the retrospective models
-retroModels <- r4ss::SSgetoutput(dirvec = file.path(here::here(new_year, "mgmt", Model_name_new),
-                                                    "retrospectives",
-                                                    paste("retro", 0:-ret_yr, sep = "")))
-
-# summarize the model results
-retroSummary <- r4ss::SSsummarize(retroModels)
-
-# Run ss3diags on retro results
-endyrvec <- retroSummary[["endyrs"]] + 0:-ret_yr
-rho_output_ss3diags <- ss3diags::SSplotRetro(retroSummary,
-                                             subplots = c("SSB"),
-                                             endyrvec = endyrvec,
-                                             legendlabels = paste("Data", 0:-ret_yr, "years"),
-                                             print = TRUE,
-                                             plotdir = here::here(new_year, "plots", "other"),
-                                             pwidth = 8.5,
-                                             pheight = 4.5)
-
-# Save output
-save(retroSummary, file = here::here(new_year, "output", "retroSummary.RData"))
-write.csv(retroSummary$SpawnBio, here::here(new_year, "output", "retro_SSB.csv"))
-write.csv(retroSummary$recruits, here::here(new_year, "output", "retro_Rec.csv"))
-write.csv(rho_output_ss3diags, here::here(new_year, "output", "retro_Rho_ss3diags.csv"))
-
-## All the r4ss retrospective stuff kinda sux but keeping it here to maybe use in future
-# r4ss::SSplotComparisons(retroSummary,
-#                         endyrvec = endyrvec,
-#                         legendlabels = paste("Data", 0:-10, "years"),
-#                         print = TRUE,
-#                         plotdir = here::here("plots", "retro"))
-# # calculate Mohn's rho
-# rho_output_r4ss <- r4ss::SSmohnsrho(summaryoutput = retroSummary,
-#                                endyrvec = endyrvec,
-#                                startyr = retroSummary[["endyrs"]] - 10,
-#                                verbose = FALSE)
-#write.csv(rho_output_r4ss, here::here("output", "retro_Rho_r4ss.csv"))
-
-
-# Run Leave-One-Out Analysis ----
-
-# Define how many LOO years you want to go back
-# loo_yr <- 1 # For testing
-loo_yr <- 10 # For full
-
-ss_datname <- list.files(here::here(new_year, "mgmt", Model_name_new), pattern = "GOAPcod")
-
-source(here::here(new_year, "R", "assessment", "LeaveOneOut.r"))
-
-LOO <- SS_doLOO(Model_name = Model_name_new,
-                newsubdir = "LeaveOneOut", 
-                years = 0:-loo_yr,
-                datafilename = ss_datname,
-                CYR = new_year,
-                run_models = TRUE)
-
-# Save output
-save(LOO, file = here::here(new_year, "output", "LOO.RData"))
-write.csv(LOO[[1]], here::here(new_year, "output", "LOO_table.csv"))
-
-# Run for newly added data - note that this is a hardwired function, where folders and data files must be manually changed to remove recent data
-LOO_add_data <- SS_doLOO_cyr(Model_name = Model_name_new,
-                             newsubdir = "LeaveOneOut",
-                             CYR = new_year,
-                             run_models = TRUE)
-
-# Save output
-save(LOO_add_data, file = here::here(new_year, "output", "LOO_add_data.RData"))
-
-
-# Run Jitter ----
-# note, in 2023 assessment couldn't get r4ss::jitter to run, but, could get the functions to run
-# so just sourcing functions and will come back to this
-
-source(here::here(new_year, "R", "assessment", "jitter.r"))
-
-# Define how many jitters you want to do
-Njitter <- 5 # For testing
-# Njitter <- 50 # For full
-
-# define a new directory
-if (!file.exists(here::here(new_year, "mgmt", Model_name_new, "jitter"))) 
-  dir.create(here::here(new_year, "mgmt", Model_name_new, "jitter"))
-
-jitter_dir <- here::here(new_year, "mgmt", Model_name_new, "jitter")
-
-# copy over the stock synthesis model files to the new directory
-r4ss::copy_SS_inputs(dir.old = here::here(new_year, "mgmt", Model_name_new), 
-                     dir.new = here::here(new_year, "mgmt", Model_name_new, "jitter"),
-                     copy_par = TRUE,
-                     copy_exe = TRUE,
-                     overwrite = TRUE)
-
-# run the jitters
-jitter_loglike <- jitter(dir = jitter_dir,
-                         Njitter = Njitter,
-                         jitter_fraction = 0.05,
-                         init_values_src = 1,
-                         exe = "ss3",
-                         printlikes = TRUE,
-                         verbose = FALSE)
-
-# Save output
-write.csv(jitter_loglike, here::here(new_year, "output", "jitter_table.csv"))
