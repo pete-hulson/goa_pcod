227dcd0ae454bb3e7b094ac867dd60877bdbc843
Matt Cheng <84427627+chengmatt@users.noreply.github.com>
2025-07-28 05:33:19 -0800

    changing starting values

--- 2025/rsch/SPoRC/Pcod_bridge_2024_env_link_tv_ageing.R
+++ 2025/rsch/SPoRC/Pcod_bridge_2024_env_link_tv_ageing.R
@@ -512,7 +512,7 @@ input_list <- Setup_Mod_Fishsel_and_Q(
                     "none_Fleet_3"),
   
   # whether to estiamte all fixed effects for fishery selectivity
-  fish_fixed_sel_pars = c("est_all",
+  fish_fixed_sel_pars_spec = c("est_all",
                           "est_all",
                           "est_all"),
   
@@ -633,14 +633,12 @@ parameters$ln_F_devs[!is.finite(parameters$ln_F_devs)] <- 0 # don't estimate f d
 # n_max_sel_blocks, n_sexes, n_fleets)
 # n_max_sel_pars = If you have 3 fleets, fleet 1 = logistic, fleet 2 = logistic, fleet 3 = double normal, this dimension will always be 6
 # n_max_sel_blocks = If you have 5 blocks for fleet 1, 3 blocks for fleet 2, and 1 block for fleet 3, this dimension will be 5.
-parameters$ln_fish_fixed_sel_pars[,1,,,1:2] <- log(60) # l50 parameter for logistic, fleet 1 and 2
+parameters$ln_fish_fixed_sel_pars[,1,,,1:2] <- log(70) # l50 parameter for logistic, fleet 1 and 2
 parameters$ln_fish_fixed_sel_pars[,2,,,1:2] <- log(1) # slope parameter for logistic, fleet 1 and 2
-parameters$ln_fish_fixed_sel_pars[,1,,,3] <- log(50) # lmax parameter for gamma, fleet 3
+parameters$ln_fish_fixed_sel_pars[,1,,,3] <- log(60) # lmax parameter for gamma, fleet 3
 parameters$ln_fish_fixed_sel_pars[,2,,,3] <- log(10) # slope for gamma fleet 3
-parameters$ln_srv_fixed_sel_pars[,1,,,] <- log(50) # lmax parameter for gamma, fleet 1
+parameters$ln_srv_fixed_sel_pars[,1,,,] <- log(60) # lmax parameter for gamma, fleet 1
 parameters$ln_srv_fixed_sel_pars[,2,,,] <- log(3) # slope for gamma fleet 1
-parameters$srv_q_coeff[,2,1] <- 0.9 # survey catchability effect
-
 
 # Fit Model ---------------------------------------------------------------
 
