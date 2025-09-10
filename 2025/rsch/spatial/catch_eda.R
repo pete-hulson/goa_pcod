9af1d66a65248df38fa73a4535010a5a2844000a
pete-hulson <pete.hulson@noaa.gov>
2025-09-09 16:24:51 -0800

    add cgoa and write output

--- 2025/rsch/spatial/catch_eda.R
+++ 2025/rsch/spatial/catch_eda.R
@@ -8,10 +8,10 @@ library(here)
 
 catch <- vroom::vroom(here::here('2025', 'data', 'raw', 'fish_catch_data.csv'))
 
-# explore data
+# explore total catch data
 catch %>% 
   tidytable::filter(trip_target_code == 'C',
-                    reporting_area_code %in% c(610, 620)) %>% 
+                    reporting_area_code %in% c(610, 620, 630)) %>% 
   tidytable::select(year, trip_target_code, fmp_gear, fmp_subarea, reporting_area_code, week_end_date, weight_posted) %>% 
   tidytable::mutate(catch_month = lubridate::month(week_end_date),
                     stanza = factor(case_when(year <= 1999 ~ '90s',
@@ -23,8 +23,7 @@ catch %>%
                     season = factor(case_when(catch_month <= 3 ~ 'Spawning',
                                               catch_month > 3 ~ 'Non-spawning'),
                                     levels = c('Spawning', 'Non-spawning'))) %>% 
-  tidytable::mutate(catch_spawn = sum(catch))
-  tidytable::summarise(catch = sum(weight_posted), .by = c(fmp_gear, reporting_area_code, stanza, season)) %>% 
-  print(n=200)
+  tidytable::summarise(catch = sum(weight_posted), .by = c(fmp_gear, fmp_subarea, reporting_area_code, stanza, season)) %>% 
+  vroom::vroom_write(., here::here('2025', 'rsch', 'spatial', 'data', 'catch_summ.csv'), delim = ',')
 
 
