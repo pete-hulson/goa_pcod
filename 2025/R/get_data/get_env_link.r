144d4311af095d2e48bbc670f6216d30bd4902d8
pete-hulson <pete.hulson@noaa.gov>
2025-11-24 15:12:02 -0900

    update env index

--- 2025/R/get_data/get_env_link.r
+++ 2025/R/get_data/get_env_link.r
@@ -1,26 +1,70 @@
 #' function to grab temperature data for longline survey catchability link
 #' 
 get_lls_env <- function(new_year,
+                        index = 'hycom',
                         ss3_frmt = TRUE,
                         var = 1){
   
-  # get cfsr data ----
-  cfsr <- vroom::vroom(here::here(new_year, 'data', 'raw_cfsr.csv'))
+  # use cfsr data ----
+  if(index == 'cfsr'){
+    cfsr <- vroom::vroom(here::here(new_year, 'data', 'raw_cfsr.csv'))
+
+    cfsr %>% 
+      dplyr::rename_all(tolower) %>% 
+      tidytable::filter(month == 6) %>% 
+      tidytable::select(year, temp = '0_20') %>% 
+      tidytable::mutate(avg = as.numeric(cfsr %>% 
+                                           dplyr::rename_all(tolower) %>% 
+                                           tidytable::filter(month == 6,
+                                                             year >= 1982,
+                                                             year <= 2012) %>% 
+                                           tidytable::select(year, temp = '0_20') %>% 
+                                           tidytable::summarise(avg_temp = mean(temp)))) %>% 
+      tidytable::mutate(index = round(temp - avg, digits = 5)) %>% 
+      tidytable::select(year, index) -> env_indx
+  }
   
-  # compute env index ----
-  cfsr %>% 
-    dplyr::rename_all(tolower) %>% 
-    tidytable::filter(month == 6) %>% 
-    tidytable::select(year, temp = '0_20') %>% 
-    tidytable::mutate(avg = as.numeric(cfsr %>% 
-                                         dplyr::rename_all(tolower) %>% 
-                                         tidytable::filter(month == 6,
-                                                           year >= 1982,
-                                                           year <= 2012) %>% 
-                                         tidytable::select(year, temp = '0_20') %>% 
-                                         tidytable::summarise(avg_temp = mean(temp)))) %>% 
-    tidytable::mutate(index = round(temp - avg, digits = 5)) %>% 
-    tidytable::select(year, index) -> env_indx
+  # use hycom-cfsr hybrid data ----
+  
+  if(index == 'hycom'){
+    hycom <- vroom::vroom(here::here(new_year, 'data', 'raw_hycom.csv'))
+    cfsr <- vroom::vroom(here::here(new_year, 'data', 'raw_cfsr.csv'))
+
+    cfsr %>% 
+      dplyr::rename_all(tolower) %>% 
+      tidytable::filter(month == 6,
+                        year >= 1979,
+                        year <= 1994) %>% 
+      tidytable::select(year, temp = '0_20') %>% 
+      tidytable::mutate(avg = as.numeric(cfsr %>% 
+                                           dplyr::rename_all(tolower) %>% 
+                                           tidytable::filter(month == 6,
+                                                             year >= 1979,
+                                                             year <= 1994) %>% 
+                                           tidytable::select(year, temp = '0_20') %>% 
+                                           tidytable::summarise(avg_temp = mean(temp)))) %>% 
+      tidytable::mutate(index = round(temp - avg, digits = 5)) %>% 
+      tidytable::select(year, index) -> env_indx1
+    
+    hycom %>% 
+      dplyr::rename_all(tolower) %>% 
+      tidytable::filter(month == 5,
+                        depth_cat == '150m') %>% 
+      tidytable::select(year, temp = mean_monthly) %>% 
+      tidytable::mutate(avg = as.numeric(hycom %>% 
+                                           dplyr::rename_all(tolower) %>% 
+                                           tidytable::filter(month == 5,
+                                                             depth_cat == '150m',
+                                                             year >= 1995,
+                                                             year <= 2012) %>% 
+                                           tidytable::select(year, temp = mean_monthly) %>% 
+                                           tidytable::summarise(avg_temp = mean(temp)))) %>% 
+      tidytable::mutate(index = round(temp - avg, digits = 5)) %>% 
+      tidytable::select(year, index) -> env_indx2
+    
+    env_indx <- env_indx1 %>% 
+      tidytable::bind_rows(env_indx2)
+  }
   
   # format for ss3 if desired ----
   if(isTRUE(ss3_frmt)){
