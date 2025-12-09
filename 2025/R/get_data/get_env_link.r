5d882a753065fc8886e38db42ae7ec2869a0f417
pete-hulson <pete.hulson@noaa.gov>
2025-12-09 10:11:03 -0900

    finalize hycom 2025 kloodge

--- 2025/R/get_data/get_env_link.r
+++ 2025/R/get_data/get_env_link.r
@@ -1,7 +1,7 @@
 #' function to grab temperature data for longline survey catchability link
 #' 
 get_lls_env <- function(new_year,
-                        index = 'hycom',
+                        index = 'cfsr',
                         ss3_frmt = TRUE,
                         var = 1){
   
@@ -21,7 +21,28 @@ get_lls_env <- function(new_year,
                                            tidytable::select(year, temp = '0_20') %>% 
                                            tidytable::summarise(avg_temp = mean(temp)))) %>% 
       tidytable::mutate(index = round(temp - avg, digits = 5)) %>% 
-      tidytable::select(year, index) -> env_indx
+      tidytable::select(year, index) -> env_indx1
+    
+    # do 2025 cloodge since cfsr not available
+    hycom <- vroom::vroom(here::here(new_year, 'data', 'raw_hycom.csv'))
+    
+    hycom %>% 
+      dplyr::rename_all(tolower) %>% 
+      # use hycom 50m as representative of cfsr
+      tidytable::filter(month == 5,
+                        depth_cat == '50m') %>% 
+      tidytable::select(year, temp = mean_monthly) %>% 
+      tidytable::mutate(index = round(temp - mean(temp[which(year <= 2012)]), digits = 5)) %>% 
+      tidytable::mutate(index2 = round(index - mean(index), digits = 5)) %>% 
+      tidytable::filter(year == 2025) %>% 
+      tidytable::mutate(index_new = round(index2 + env_indx1 %>% 
+                          tidytable::summarise(mu_ind = mean(index)) %>% 
+                          tidytable::pull(mu_ind), digits = 5)) %>% 
+      tidytable::select(year, index = index_new) -> env_indx2
+    
+    env_indx <- env_indx1 %>% 
+      tidytable::bind_rows(env_indx2)
+    
   }
   
   # use hycom-cfsr hybrid data ----
@@ -32,7 +53,7 @@ get_lls_env <- function(new_year,
 
     cfsr %>% 
       dplyr::rename_all(tolower) %>% 
-      tidytable::filter(month == 6) %>% 
+      tidytable::filter(month == 5) %>% 
       tidytable::select(year, temp = '0_20') %>% 
       tidytable::mutate(index = round(temp - mean(temp[which(year >= 1982 & year <= 2012)]), digits = 5)) %>% 
       tidytable::select(year, index) %>% 
